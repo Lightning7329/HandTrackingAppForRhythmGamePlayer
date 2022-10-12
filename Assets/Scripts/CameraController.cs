ac546e2d054e1857336b5dd61aea82687b323ba8
watanabe_konosuke <trevelink.zelda113@gmail.com>
2022-10-13 01:51:01 +0900

    カメラのリセットボタンを追加

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
     Transform cameraTransform;
+    Button camera1;
     [SerializeField] private float moveSpead = 0.02f;
     [SerializeField] private float rotateSpead = 0.07f;
     [SerializeField] private float zoomSpead = 0.03f;
@@ -12,6 +14,8 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         cameraTransform = GetComponent<Transform>();
+        camera1 = GameObject.Find("Camera1").GetComponent<Button>();
+        camera1.onClick.AddListener(OnBtn_Camera1);
     }
 
     void Update()
@@ -21,6 +25,9 @@ public class CameraController : MonoBehaviour
         Zoom();
     }
 
+    /// <summary>
+    /// カメラの平行移動
+    /// </summary>
     private void TransformPos()
     {
         if (Input.GetKey(KeyCode.W)) cameraTransform.position += new Vector3(0.0f, 0.0f, moveSpead);
@@ -29,6 +36,9 @@ public class CameraController : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) cameraTransform.position += new Vector3(moveSpead, 0.0f, 0.0f);
     }
 
+    /// <summary>
+    /// カメラのローカル座標周りの回転
+    /// </summary>
     private void TransformRot()
     {
         Vector3 ang = Vector3.zero;
@@ -39,9 +49,21 @@ public class CameraController : MonoBehaviour
         cameraTransform.Rotate(ang, Space.Self);
     }
 
+    /// <summary>
+    /// カメラのズームイン/アウト
+    /// </summary>
     private void Zoom()
     {
         if (Input.GetKey(KeyCode.F)) cameraTransform.position -= transform.forward * zoomSpead;
         else if (Input.GetKey(KeyCode.N)) cameraTransform.position += transform.forward * zoomSpead;
+    }
+
+    /// <summary>
+    /// Camera1ボタンが押されたときに実行。カメラ位置をデフォルトに戻す。
+    /// </summary>
+    private void OnBtn_Camera1()
+    {
+        cameraTransform.position = new Vector3(0.0f, 13.51f, -7.34f);
+        cameraTransform.rotation = Quaternion.Euler(new Vector3(60.27f, 0.0f, 0.0f));
     }
 }
