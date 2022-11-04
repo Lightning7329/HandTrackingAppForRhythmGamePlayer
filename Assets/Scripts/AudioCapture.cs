64caa87865fa11040cae726fef4f53463d5f9642
Sakailab <watanabe_ac27@toki.waseda.jp>
2022-11-04 18:04:52 +0900

    外部入力でキャプチャした映像の音声をAudioSourceから再生することに成功

---
+++ Assets/Scripts/AudioCapture.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KW_Mocap
+{
+    public class AudioCapture : MonoBehaviour
+    {
+        [SerializeField] private string m_DeviceName;
+        private AudioClip m_AudioClip;
+        private AudioSource m_MicAudioSource;
+
+        void Awake()
+        {
+            m_MicAudioSource = GetComponent<AudioSource>();
+        }
+        void Start()
+        {
+            string targetDevice = "";
+
+            foreach (var device in Microphone.devices)
+            {
+                Debug.Log($"Device Name: {device}");
+                if (device.Contains(m_DeviceName))
+                {
+                    targetDevice = device;
+                }
+            }
+
+            Debug.Log($"=== Device Set: {targetDevice} ===");
+            m_AudioClip = Microphone.Start(targetDevice, true, 10, 48000);
+            m_MicAudioSource.clip = m_AudioClip;
+            m_MicAudioSource.Play();
+        }
+    }
+}
