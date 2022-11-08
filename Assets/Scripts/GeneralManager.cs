f7577ea9de4b49e847c978d266ac5056ca3146dc
watanabe_konosuke <trevelink.zelda113@gmail.com>
2022-11-08 20:56:59 +0900

    GeneralManagerの作成

---
+++ Assets/Scripts/GeneralManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace KW_Mocap
+{
+    public class GeneralManager : MonoBehaviour
+    {
+        public static GeneralManager I = null;
+
+        void Awake()
+        {
+            if (I == null)
+            {
+                I = this;
+                DontDestroyOnLoad(this.gameObject); //シーンの移動で破棄されない
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            //WorldTimer.DisplayFrameCount();
+            //WorldTimer.Run();
+        }
+    }
+}
