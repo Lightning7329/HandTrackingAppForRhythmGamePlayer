53ea0d829bec02ef98c35a70ce104c57d4d1ad2b
watanabe_konosuke <trevelink.zelda113@gmail.com>
2022-11-08 20:57:59 +0900

    モーションデータのセーブとロードに関するスクリプトを作成

--- Assets/Scripts/Hand/MotionData.cs
+++ Assets/Scripts/Hand/MotionData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,125 @@ namespace KW_Mocap
     public class MotionData
     {
         public HandData left, right;
+
+        /// <summary>
+        /// byte配列からモーションデータを復元するコンストラクタ
+        /// </summary>
+        /// <param name="buf">1フレーム分のモーションデータを格納したbyte配列</param>
+        /// <param name="offset">左手か右手かでbyte配列の読み取り位置が異なる</param>
+        public MotionData(byte[] buf)
+        {
+            left = new HandData(buf, HandData.Offset.Left);
+            right = new HandData(buf, HandData.Offset.Right);
+        }
+
+        /// <summary>
+        /// 受け取ったbyte配列にモーションデータをセットする
+        /// </summary>
+        /// <param name="buf">長さ144以上のbyte配列</param>
+        public void SetBytes(byte[] buf)
+        {
+            left.SetBytes(buf, HandData.Offset.Left);
+            right.SetBytes(buf, HandData.Offset.Right);
+        }
     }
 
     public class HandData
     {
-        public Transform palm;
-        public Quaternion[] quaternions = new Quaternion[10];
+        /// <summary>
+        /// 手のモーションデータのうち左か右かでバッファ読み取り時のオフセットが変わる
+        /// </summary>
+        public enum Offset { Left = 0, Right = 72 };
+
+        /// <summary>
+        /// 手のひらの位置の座標。Transform.Positionに入れる。
+        /// </summary>
+        public Vector3 palmPos;
+
+        /// <summary>
+        /// 手のひらの回転のクォータニオン。Tranform.Rotationに入れる。
+        /// </summary>
+        public Quaternion palmRot;
+
+        /// <summary>
+        /// 指の関節の回転のクォータニオンの配列。Tranform.Rotationに入れる。
+        /// </summary>
+        public Quaternion[] jointRot = new Quaternion[10];
+
+        /// <summary>
+        /// byte配列から片手分のモーションデータを復元するコンストラクタ
+        /// </summary>
+        /// <param name="buf">1フレーム分のモーションデータを格納したbyte配列</param>
+        /// <param name="offset">左手か右手かでbyte配列の読み取り位置が異なる</param>
+        public HandData(byte[] buf, Offset offset)
+        {
+            palmPos = Vector3.zero;
+            palmRot = Quaternion.identity;
+
+            // 手のひらの位置と回転
+            palmPos.x = BitConverter.ToSingle(buf, (int)offset);
+            palmPos.y = BitConverter.ToSingle(buf, 4 + (int)offset);
+            palmPos.z = BitConverter.ToSingle(buf, 8 + (int)offset);
+            palmRot.w = BitConverter.ToSingle(buf, 12 + (int)offset);
+            palmRot.x = BitConverter.ToSingle(buf, 16 + (int)offset);
+            palmRot.y = BitConverter.ToSingle(buf, 20 + (int)offset);
+            palmRot.z = BitConverter.ToSingle(buf, 24 + (int)offset);
+
+            // 関節の回転
+            for (int i = 0; i < jointRot.Length; i++)
+            {
+                jointRot[i] = Quaternion.identity;
+
+                int shift = 4 * i + (int)offset;
+                jointRot[i].w = BitConverter.ToSingle(buf, 28 + shift);
+                jointRot[i].x = BitConverter.ToSingle(buf, 29 + shift);
+                jointRot[i].y = BitConverter.ToSingle(buf, 30 + shift);
+                jointRot[i].z = BitConverter.ToSingle(buf, 31 + shift);
+            }
+        }
+
+        /// <summary>
+        /// 受け取ったbyte配列に片手分のモーションデータをシリアライズして格納する
+        /// </summary>
+        /// <param name="buf"></param>
+        /// <param name="offset"></param>
+        public void SetBytes(byte[] buf, Offset offset)
+        {
+            // 手のひらの位置と回転
+            SetByteBuf(BitConverter.GetBytes(palmPos.x), buf, (int)offset, 4);
+            SetByteBuf(BitConverter.GetBytes(palmPos.y), buf, 4 + (int)offset, 4);
+            SetByteBuf(BitConverter.GetBytes(palmPos.z), buf, 8 + (int)offset, 4);
+            SetByteBuf(BitConverter.GetBytes(palmRot.w), buf, 12 + (int)offset, 4);
+            SetByteBuf(BitConverter.GetBytes(palmRot.x), buf, 16 + (int)offset, 4);
+            SetByteBuf(BitConverter.GetBytes(palmRot.y), buf, 20 + (int)offset, 4);
+            SetByteBuf(BitConverter.GetBytes(palmRot.z), buf, 24 + (int)offset, 4);
+            // ここまでで配列bufのbuf[0 + offset]からbuf[27 + offset]まで使用
+
+            // 関節の回転
+            // ここで配列bufのbuf[28 + offset]からbuf[71 + offset]まで使用（31+4*9+4=71）
+            for (int i = 0; i < jointRot.Length; i++)
+            {
+                int shift = 4 * i + (int)offset;
+                SetByteBuf(BitConverter.GetBytes(jointRot[i].w), buf, 28 + shift, 4);
+                SetByteBuf(BitConverter.GetBytes(jointRot[i].x), buf, 29 + shift, 4);
+                SetByteBuf(BitConverter.GetBytes(jointRot[i].y), buf, 30 + shift, 4);
+                SetByteBuf(BitConverter.GetBytes(jointRot[i].z), buf, 31 + shift, 4);
+            }
+        }
+
+        /// <summary>
+        /// byteDataからdataCount個の数値ををbufのoffset番目から代入する
+        /// </summary>
+        /// <param name="byteData">bufに入れたいbyte配列</param>
+        /// <param name="buf">バッファ用byte配列</param>
+        /// <param name="offset">bufに入れ始めるindex</param>
+        /// <param name="dataCount">bufに記録する数値の数</param>
+        private void SetByteBuf(byte[] byteData, byte[] buf, int offset, int dataCount)
+        {
+            for (int i = 0; i < dataCount; i++)
+            {
+                buf[offset + i] = byteData[i];
+            }
+        }
     }
 }
---
+++ Assets/Scripts/RecordData.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace KW_Mocap
+{
+    public class RecordData : MonoBehaviour
+    {
+        const int MaxDataCount = 10000;   //fps30で5分ちょっと
+
+        int recordDataCount;
+        MotionData[] motionData = new MotionData[MaxDataCount];
+        bool isRecording = false;
+
+        void Update()
+        {
+            if (isRecording) Record();
+        }
+
+        void Record()
+        {
+            // motionData[recordDataCount]にデータを入れていく
+            recordDataCount++;
+        }
+
+        public void StartRecording()
+        {
+            if (isRecording) return;
+            isRecording = true;
+            recordDataCount = 0;
+        }
+
+        public void StopRecording()
+        {
+            if (!isRecording) return;
+            isRecording = false;
+        }
+
+        public void Save(String fileName)
+        {
+            if (isRecording) return;
+
+            byte[] buf = new byte[144];
+            try
+            {
+                using (FileStream fs = new FileStream($"SavedMotionData/{fileName}.bin", FileMode.CreateNew, FileAccess.Write))
+                {
+                    // 記録するデータ点数
+                    int savingDataCount = recordDataCount < MaxDataCount ? recordDataCount : MaxDataCount;
+                    byte[] byte_DataCount = BitConverter.GetBytes(savingDataCount);
+                    fs.Write(byte_DataCount, 0, byte_DataCount.Length);
+
+                    for (int i = 0; i < savingDataCount; i++)
+                    {
+                        //データをシリアル化してFileStreamに書き込み
+                        motionData[i].SetBytes(buf);
+                        fs.Write(buf, 0, 144);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Debug.Log(e);
+            }
+        }
+
+        public void Load(String fileName)
+        {
+            if (isRecording) return;
+
+            byte[] buf = new byte[144];
+            try
+            {
+                using (FileStream fs = new FileStream($"SavedMotionData/{fileName}.bin", FileMode.Open, FileAccess.Read))
+                {
+                    // 読み込むデータ点数
+                    fs.Read(buf, 0, 4);
+                    recordDataCount = BitConverter.ToInt32(buf, 0);
+
+                    for (int i = 0; i < recordDataCount; i++)
+                    {
+                        fs.Read(buf, 0, 144);
+                        motionData[i] = new MotionData(buf);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+}
