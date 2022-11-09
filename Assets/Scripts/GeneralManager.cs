3ed6490fbdc9917f49550ab4dd62e6ae91efcc7b
watanabe_konosuke <trevelink.zelda113@gmail.com>
2022-11-09 16:39:36 +0900

    VideoControllerに持たせていたUI繋がりのメソッドをGeneralManagerへ移動

--- Assets/Scripts/GeneralManager.cs
+++ Assets/Scripts/GeneralManager.cs
@@ -1,16 +1,27 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.InteropServices.ComTypes;
-using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace KW_Mocap
 {
     public class GeneralManager : MonoBehaviour
     {
         public static GeneralManager I = null;
+
+        bool isPlaying = false;
+        private float currentSpeed = 1.0f;
+        [SerializeField] private float speedChange = 0.05f;
+        [SerializeField] private int skipSeconds = 5;
+
+        MotionPlayer motionPlayer = null;
+        MotionRecoder motionRecoder = null;
+
+        VideoController videoController = null;
+
+        // uGUI側
+        private Text txt_speed;
+        private Button playButton, forwardButton, backwardButton, addSpeedButton, subSpeedButton;
 
         void Awake()
         {
@@ -29,6 +40,97 @@ namespace KW_Mocap
         {
             //WorldTimer.DisplayFrameCount();
             //WorldTimer.Run();
+            //motionPlayer = GameObject.Find("Hands").GetComponent<MotionPlayer>();
+
+            // video側
+            videoController = GameObject.Find("Display for Play").GetComponent<VideoController>();
+
+            // uGUI側
+            SetButton(ref playButton, "PlayButton", OnBtn_Play, "Play");
+            SetButton(ref forwardButton, "ForwardButton", OnBtn_Forward, $"{skipSeconds}s");
+            SetButton(ref backwardButton, "BackwardButton", OnBtn_Backward, $"{skipSeconds}s");
+            SetButton(ref addSpeedButton, "AddSpeedButton", OnBtn_AddSpeed, "+0.05");
+            SetButton(ref subSpeedButton, "SubSpeedButton", OnBtn_SubSpeed, "-0.05");
+            txt_speed = GameObject.Find("Speed").transform.Find("Text").gameObject.GetComponent<Text>();
+            txt_speed.text = "x1.00";
+        }
+
+        void OnBtn_Play()
+        {
+            // motion側
+            if (motionPlayer != null)
+            {
+                string fileName = "Sample";
+                MotionRecoder motionRecorder = new MotionRecoder();
+                MotionData[] motionData = motionRecorder.Load(fileName);
+
+                motionPlayer.SetMotionData(motionData);
+            }
+
+            // video側
+            videoController.TogglePlayAndPause();
+
+            // uGUI側
+            Text t = playButton.transform.Find("Text").GetComponent<Text>();
+            if (isPlaying)
+            {
+                t.text = "Play";
+                isPlaying = false;
+            }
+            else
+            {
+                t.text = "Pause";
+                isPlaying = false;
+            }
+        }
+
+        void OnBtn_Forward()
+        {
+            // motion側
+            // TODO
+
+            // video側
+            videoController.Skip(skipSeconds);
+        }
+
+        void OnBtn_Backward()
+        {
+            // motion側
+            // TODO
+
+            // video側
+            videoController.Skip(-skipSeconds);
+        }
+
+        void OnBtn_AddSpeed()
+        {
+            // motion側
+            // TODO
+
+            // video側
+            videoController.ChangeSpeed(currentSpeed += speedChange);
+
+            // uGUI側
+            txt_speed.text = $"x{currentSpeed:F2}";
+        }
+
+        void OnBtn_SubSpeed()
+        {
+            // motion側
+            // TODO
+
+            // video側
+            videoController.ChangeSpeed(currentSpeed -= speedChange);
+
+            // uGUI側
+            txt_speed.text = $"x{currentSpeed:F2}";
+        }
+
+        private void SetButton(ref Button button, string name, UnityEngine.Events.UnityAction call, string text)
+        {
+            button = GameObject.Find(name).GetComponent<Button>();
+            button.onClick.AddListener(call);
+            button.transform.Find("Text").GetComponent<Text>().text = text;
         }
     }
 }
--- Assets/Scripts/Video/VideoController.cs
+++ Assets/Scripts/Video/VideoController.cs
@@ -9,12 +9,7 @@ namespace KW_Mocap
     public class VideoController : MonoBehaviour
     {
         private VideoPlayer video;
-        private Text txt_speed;
-        private Button playButton, forwardButton, backwardButton, addSpeedButton, subSpeedButton;
-        private bool isPlaying = false;
-        [SerializeField] private int skipSeconds = 5;
-        private float currentSpeed = 1.0f;
-        [SerializeField] private float speedChange = 0.05f;
+        public bool isPlaying { get; private set; } = false;
 
         void Start()
         {
@@ -25,73 +20,33 @@ namespace KW_Mocap
             //Debug.Log("lengh: " + video.length + "s");
             //Debug.Log("frame rate: " + video.frameRate);
             //Debug.Log("frame count: " + video.frameCount);
-            SetButton(ref playButton, "PlayButton", OnBtn_playButton, "Play");
-            SetButton(ref forwardButton, "ForwardButton", OnBtn_Forward, $"{skipSeconds}s");
-            SetButton(ref backwardButton, "BackwardButton", OnBtn_Backward, $"{skipSeconds}s");
-            SetButton(ref addSpeedButton, "AddSpeedButton", OnBtn_AddSpeed, "+0.05");
-            SetButton(ref subSpeedButton, "SubSpeedButton", OnBtn_SubSpeed, "-0.05");
-            txt_speed = GameObject.Find("Speed").transform.Find("Text").gameObject.GetComponent<Text>();
-            txt_speed.text = "x1.00";
         }
-
-        //void Update()
-        //{
-
-        //}
 
         /// <summary>
         /// 再生ボタンを押したときに実行。再生中の場合は一時停止、一時停止中の場合は再生する。
         /// </summary>
-        void OnBtn_playButton()
+        public void TogglePlayAndPause()
         {
-            Text t = playButton.transform.Find("Text").GetComponent<Text>();
             if (isPlaying)
             {
                 video.Pause();
-                t.text = "Play";
                 isPlaying = false;
             }
             else
             {
                 video.Play();
-                t.text = "Pause";
                 isPlaying = true;
             }
         }
 
-        /// <summary>
-        /// FBSeconds 秒進む
-        /// </summary>
-        void OnBtn_Forward()
+        public void Skip(int seconds)
         {
-            video.frame += skipSeconds * (long)video.frameRate;
+            video.frame += seconds * (long)video.frameRate;
         }
 
-        /// <summary>
-        /// FBSeconds 秒戻る
-        /// </summary>
-        void OnBtn_Backward()
+        public void ChangeSpeed(float speed)
         {
-            video.frame -= skipSeconds * (long)video.frameRate;
-        }
-
-        void OnBtn_AddSpeed()
-        {
-            video.playbackSpeed = currentSpeed += speedChange;
-            txt_speed.text = $"x{currentSpeed:F2}";
-        }
-
-        void OnBtn_SubSpeed()
-        {
-            video.playbackSpeed = currentSpeed -= speedChange;
-            txt_speed.text = $"x{currentSpeed:F2}";
-        }
-
-        private void SetButton(ref Button button, string name, UnityEngine.Events.UnityAction call, string text)
-        {
-            button = GameObject.Find(name).GetComponent<Button>();
-            button.onClick.AddListener(call);
-            button.transform.Find("Text").GetComponent<Text>().text = text;
+            video.playbackSpeed = speed;
         }
     }
 }
