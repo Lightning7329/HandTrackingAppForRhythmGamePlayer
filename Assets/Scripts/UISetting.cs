5f3f4bd46b646d866b0c15b37a0fa3159502f147
Sakailab <watanabe_ac27@toki.waseda.jp>
2022-12-14 05:47:31 +0900

    UISettingクラスの拡張

--- Assets/Scripts/UISetting.cs
+++ Assets/Scripts/UISetting.cs
@@ -20,6 +20,16 @@ namespace KW_Mocap
             button.GetComponentInChildren<Text>().text = text;
         }
 
+        public static void SetButton(ref Button button, Transform parent, string name, UnityAction call)
+        {
+            button = parent.Find(name).GetComponent<Button>();
+            if (button == null)
+            {
+                Debug.Log($"child object {name} could not be found.");
+            }
+            button.onClick.AddListener(call);
+        }
+
         public static void SetButton(GameObject buttonObject, UnityAction call, string text)
         {
             buttonObject.GetComponent<Button>().onClick.AddListener(call);
@@ -29,6 +39,11 @@ namespace KW_Mocap
         public static void SetButtonText(GameObject buttonObject, string text)
         {
             buttonObject.GetComponentInChildren<Text>().text = text;
+        }
+
+        public static void SetButtonColor(Button button, Color color)
+        {
+            button.GetComponent<Image>().color = color;
         }
 
         public static void AddEventTrigger(EventTrigger eventTrigger, EventTriggerType triggerType, UnityAction<BaseEventData> call)
